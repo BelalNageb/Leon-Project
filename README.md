06ad4321afd134a11cfe13b8cdb55fffa358a94d
Belal Nageb <belalnageb466@gmail.com>
2025-01-18 06:29:57 +0200

    Added scroll progress bar and Added a scroll-to-top button with smooth scrolling , Update Description README.file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Leon is a free PSD template made by Graphberry. It comes in a modern, flat design with vibrant colors. This landing page is great for representing your portfolio or other types of websites. Free Google font used, the link is in "help" file
 
-### Description:
+#### Description:
 
 A modern, responsive web design project built with HTML and CSS. It features a clean layout with a grid system, consistent styling using CSS variables, and full responsiveness across all screen sizes through media queries.
 The project includes the following sections:
@@ -12,15 +12,25 @@ The project includes the following sections:
 - Services
 - Portfolio
 
-## Add javascript file to handle navigation and interactive elements
+### Add javascript file to handle navigation and interactive elements
 
-### Description:
+1. #### to handle navigation and interactive elements
+
+##### Description:
 
 - Added a main.js file to manage dynamic navigation behaviors and smooth scrolling functionality across sections of the website.
 - Implemented interactive hover effects on the navigation menu icon.
 - Created click event listeners for the main menu links to toggle visibility and provide smooth scrolling to respective sections (Services, Portfolio, About, Contact).
 - Integrated a window load event to smoothly scroll to specific sections based on URL hash, ensuring a seamless user experience upon page load or refresh.
 
-### Demo
+2. #### Add scroll progress bar that fills width based on page scroll position
+
+- Added a fixed progress bar at the top of the page.
+- The progress bar width updates dynamically as the user scrolls down the page.
+- Uses JavaScript to calculate the scroll position and CSS to style the progress bar.
+
+3. #### Added a scroll-to-top button with smooth scrolling functionality and styled it for better user experience."
+
+## Demo
 
 <belalnageb.github.io/Leon-Project/>
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// // manage dynamic navigation behaviors and smooth scrolling functionality across sections of the website
 let myLinks = document.querySelector(".links");
 let myUl = document.querySelector(".links ul");
 let mySecondSpan = document.querySelector(".links .icon span:nth-child(2)");
 let myIcon = document.querySelector(".icon");
-
+// Implemented interactive hover effects on the navigation menu icon.
 myIcon.addEventListener("mouseleave", function () {
   mySecondSpan.style.width = "60%";
 });
@@ -23,7 +24,7 @@ myLinks.addEventListener("click", function (event) {
 document.addEventListener("click", function () {
   myUl.style.display = "none";
 });
-
+// Created click event listeners for the main menu links to toggle visibility and provide smooth scrolling to respective sections
 let servicesLink = document.querySelector(".services-link");
 let portfolioLink = document.querySelector(".portfolio-link");
 let aboutLink = document.querySelector(".about-link");
@@ -53,7 +54,7 @@ contactLink.addEventListener("click", function (event) {
     behavior: "smooth",
   });
 });
-
+// Integrated a window load event to smoothly scroll to specific sections based on URL hash, ensuring a seamless user experience upon page load or refresh.
 window.addEventListener("load", function () {
   if (!window.location.hash) {
     setTimeout(function () {
@@ -66,4 +67,32 @@ window.addEventListener("load", function () {
       });
     }, 0);
   }
+});
+
+// The progress bar width updates dynamically as the user scrolls down the page.
+window.addEventListener("scroll", () => {
+  let scrollElement = document.querySelector(".scroll-progress");
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollTop = window.scrollY;
+  scrollElement.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+// Added a scroll-to-top button with smooth scrolling functionality and styled it for better user experience.
+let scrollUpBotton = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollUpBotton.style.display = "flex";
+  } else {
+    scrollUpBotton.style.display = "none";
+  }
+});
+
+scrollUpBotton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
